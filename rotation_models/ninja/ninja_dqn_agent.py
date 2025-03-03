fixed delay to softmax

rotation_models/ninja/ninja_dqn_agent.py
@@ -27,19 +27,20 @@ class NinjaDQNAgent:
         self.delay_model = self._build_delay_model()
         self.gcd_model = self._build_gcd_model()
         self.ogcd_model = self._build_ogcd_model()
+        self.time_unit = 10
 
     def _build_delay_model(self):
         inp = keras.layers.Input(shape=(self.state_size, ))
         dense = keras.layers.Dense(24, name='dense1')(inp)
         dense2 = keras.layers.Dense(24, name='dense2')(dense)
-        delay_output = keras.layers.Dense(1, activation='sigmoid')(dense2)
+        delay_output = keras.layers.Dense(self.time_unit, activation='softmax')(dense2)
 
         model = keras.Model(inputs=inp, outputs=delay_output)
 
         model.compile(
             optimizer=keras.optimizers.Nadam(
                 learning_rate=self.learning_rate),
-            loss='binary_crossentropy',
+            loss='categorical_crossentropy',
         )
 
         return model
@@ -85,7 +86,14 @@ class NinjaDQNAgent:
         self.memory.append((state, action, reward, next_state, done, is_gcd))
 
     def act1(self, state, is_gcd):
-        return 0 if is_gcd else self.delay_model.predict(tf.reshape(state, (1, -1)), verbose=0)[0][0]
+        if is_gcd:
+            return 0
+        else:
+            if np.random.rand() <= self.epsilon:
+                return np.random.randint(0, 10)
+
+            delay_values = self.delay_model.predict(tf.reshape(state, (1, -1)), verbose=0)
+            return np.argmax(delay_values)
 
     def act_skill(self, state, valid_actions=None):
         """ Select next GCD 
@@ -110,26 +118,27 @@ class NinjaDQNAgent:
         return np.random.choice(valid_actions)
 
     def replay(self, batch_size):
-        # 경험 리플레이를 통한 학습
         if len(self.memory) < batch_size:
             return
 
         minibatch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done, is_gcd in minibatch:
+        for state, action_delay, action_skill, reward, next_state, done, is_gcd in minibatch:
             state_input = tf.reshape(state, (1, -1))
             target = reward
 
             if is_gcd:
                 target_f1 = self.gcd_model.predict(state_input, verbose=0)
-                target_f1[0][action] = target
+                target_f1[0][action_skill] = target
                 self.gcd_model.fit(state_input, target_f1,
                                    epochs=1, verbose=0)
             else:
+                target_delay = self.delay_model.predict(state_input, verbose=0)
+                target_delay[0][action_delay] = target
+                
                 target_f1 = self.ogcd_model.predict(state_input, verbose=0)
-                target_f1[0][action] = target
+                target_f1[0][action_skill] = target
 
-                self.delay_model.fit(state_input, np.array(
-                    [[target]]), epochs=1, verbose=0)
+                self.delay_model.fit(state_input, target_delay, epochs=1, verbose=0)
                 self.ogcd_model.fit(state_input, target_f1,
                                     epochs=1, verbose=0)
 